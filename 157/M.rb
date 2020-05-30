c16f7236e21c50ef99628437e4bd605a762ea413
Watanabe Hiroki <x5dwimpej@yahoo.co.jp>
2020-05-30 09:44:17 +0900

    157AB

---
+++ 157/M.rb
@@ -0,0 +1,11 @@
+a = [1, 2, 3, 4]
+
+count = 0
+
+a.each do |i|
+  if i == 1
+    count += 1
+  end
+end
+
+puts count
---
+++ 157/N.rb
@@ -0,0 +1,8 @@
+a = [1, 2, 3, 4]
+
+a.each do |i|
+  if i == 2
+    puts i
+    break
+  end
+end
